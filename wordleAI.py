baf5bce25f57e78cddf7fa360d9dbd13afa63c89
seye42 <24215757+seye42@users.noreply.github.com>
2022-02-05 12:28:31 -0500

    Refactored into functions

--- wordleAI.py
+++ wordleAI.py
@@ -61,47 +61,47 @@ def filterByPositionedLetters(allWords, positionedLetters):
     return filteredWords
 
 
-# def buildLetterDictionary(word):
-#     dictionary = {}
-#     for letter in word:
-#         if letter not in dictionary.keys():
-#             dictionary[letter] = 1
-#         else:
-#             dictionary[letter] += 1
-#     return dictionary
+def initializeWordList(gameWordLength=5):
+    # load the dictionary of all valid words
+    allWords = loadWordList(r'dictionary.txt')
+
+    # filter down to only the five letter long words
+    return filterByLength(allWords, gameWordLength)
 
 
-# load the dictionary of all valid words
-allWords = loadWordList(r'dictionary.txt')
+def play():
+    gameWordLength = 5
+    gameWords = initializeWordList(gameWordLength)
+    initWordCount = len(gameWords)
 
-# filter down to only the five letter long words
-gameWordLength = 6
-gameWords = filterByLength(allWords, gameWordLength)
+    while True:
+        # prompt for game state elements
+        excLetters = input('excluded:      ')
+        misLetters = input('mispositioned: ')
+        posLetters = input('positioned:    ')
 
-while True:
-    # prompt for game state elements
-    excLetters = input('excluded:      ')
-    misLetters = input('mispositioned: ')
-    posLetters = input('positioned:    ')
+        # condition inputs
+        excLetters = ''.join(sorted(excLetters.upper()))  # sort and uppercase
+        misLetters = misLetters.upper()
+        if len(misLetters) != gameWordLength:  # malformed, prompt again
+            continue
+        incLetters = misLetters.replace('?', '')
+        posLetters = posLetters.upper()
+        if len(posLetters) != gameWordLength:  # malformed, prompt again
+            continue
 
-    # condition inputs
-    excLetters = ''.join(sorted(excLetters.upper()))  # sort and uppercase
-    misLetters = misLetters.upper()
-    if len(misLetters) != gameWordLength:  # malformed, prompt again
-        continue
-    incLetters = misLetters.replace('?', '')
-    posLetters = posLetters.upper()
-    if len(posLetters) != gameWordLength:  # malformed, prompt again
-        continue
+        # TODO: consider cross-validating inputs for consistency:
+        #       * non-empty intersection of include and exclude sets
+        #       * non-? in positional string are consistent with include and exclude sets
 
-    # TODO: consider cross-validating inputs for consistency:
-    #       * non-empty intersection of include and exclude sets
-    #       * non-? in positional string are consistent with include and exclude sets
+        # apply filters for excluded letters, included letters, and position-fixed letters
+        prevWordCount = len(gameWords)
+        gameWords = filterByExcludedLetters(gameWords, excLetters)
+        gameWords = filterByIncludedLetters(gameWords, incLetters)
+        gameWords = filterByMispositionedLetters(gameWords, misLetters)
+        gameWords = filterByPositionedLetters(gameWords, posLetters)
+        print(gameWords)
+        print('%d/%d words left, %0.1f%% reduction in last play' \
+              % (len(gameWords), initWordCount, float(prevWordCount - len(gameWords)) / prevWordCount * 100.0))
 
-    # apply filters for excluded letters, included letters, and position-fixed letters
-    prevWordCount = len(gameWords)
-    gameWords = filterByExcludedLetters(gameWords, excLetters)
-    gameWords = filterByIncludedLetters(gameWords, incLetters)
-    gameWords = filterByMispositionedLetters(gameWords, misLetters)
-    gameWords = filterByPositionedLetters(gameWords, posLetters)
-    print(gameWords)
+    return
